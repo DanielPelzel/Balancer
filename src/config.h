d88a8d7d694d4bacab34fa54f40189183ef8a857
DanielPelzel <pelzel.daniel@gmail.com>
2026-05-03 21:40:49 +0200

    add class structure and deleted test code

---
+++ src/config.h
@@ -0,0 +1,28 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+//Pin Belegungen
+//In
+#define In4 26
+#define In3 25
+#define In2 33
+#define  In1 32
+
+//PWM
+#define ENB 14
+#define ENA 13
+
+//Encoder rechts
+#define Enc_A_R 36
+#define Enc_B_R 39
+
+//Encoder links
+#define Enc_A_L 35
+#define Enc_B_L 34
+
+//MPU 6050
+#define SCL 22
+#define SDA 21
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,11 @@
 #include <Arduino.h>
+#include "config.h"
 
-//In
-#define In4 26
-#define In3 25
-#define In2 33
-#define  In1 32
-
-//PWM
-#define ENB 14
-#define ENA 13
-
-//Encoder rechts
-#define Enc_A_R 36
-#define Enc_B_R 39
-
-//Encoder links
-#define Enc_A_L 35
-#define Enc_B_L 34
-
-//MPU 6050
-#define SCL 22
-#define SDA 23
 
 
 void setup() {
     Serial.begin(115200);
+
     ledcSetup(0, 1000, 8); //Kanal 0 mit 1000er Frequenz und 8 bit
     ledcSetup(1, 1000, 8);
 
@@ -36,16 +17,10 @@ void setup() {
     pinMode(In3, OUTPUT);
     pinMode(In4, OUTPUT);
 
-
-
     pinMode(Enc_A_R, INPUT);
     pinMode(Enc_B_R, INPUT);
     pinMode(Enc_A_L, INPUT);
     pinMode(Enc_B_L, INPUT);
-
-
-
-
 }
 
 
@@ -88,13 +63,5 @@ void backward(int speed) {
 
 
 void loop() {
-    forward(255);
-    delay(5000);
-    stop();
-    delay(5000);
-    backward(255);
-    delay(5000);
-    stop();
-    delay(5000);
 
 }
